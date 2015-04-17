4dfaa93ca74bccd2bd517dde5c3ad87388554682
TimKuan <ghx10086@gmail.com>
2015-04-18 02:03:10 +0800

    first

---
+++ app/config_user.php
@@ -0,0 +1,10 @@
+<?
+define('BASE','?/');
+define('SEED','6ggiwk40');
+$db_config = array(
+  'host'      =>'localhost', 
+  'user'      =>'root',  
+  'password'  =>'leokuan', 
+  'db_type'   =>'mysql',
+  'default_db'=>'eku'
+);
